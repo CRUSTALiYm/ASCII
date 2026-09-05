9a6ac4f086fd8e015e2bfa9d01375c4a2029f7ea
CRUSTALiYm <91287500+CRUSTALiYm@users.noreply.github.com>
2026-09-05 17:42:54 +0400

    clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { invoke } from "@tauri-apps/api/core";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
-}
+export class AppComponent {}
