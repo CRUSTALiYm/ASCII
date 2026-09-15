b282c472159472b2f74d797bcc76aeff83301612
CRUSTALiYm <91287500+CRUSTALiYm@users.noreply.github.com>
2026-09-15 14:33:05 +0400

    fix

--- src/app/ascii-converter/advanced-settings/advanced-settings.component.ts
+++ src/app/ascii-converter/advanced-settings/advanced-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { SelectModule } from "primeng/select";
 import { TagModule } from "primeng/tag";
@@ -47,7 +47,6 @@ interface ComputeBackendOption {
   ],
   templateUrl: "./advanced-settings.component.html",
   styleUrl: "./advanced-settings.component.scss",
-  encapsulation: ViewEncapsulation.None,
 })
 export class AdvancedSettingsComponent {
   readonly settings = inject(SettingsService);
